7e0775d1e4c53ca08998f2c71302cab2ff1f1680
Sandra Passini Soares <sandrinha_passini@hotmail.com>
2023-08-20 19:31:47 -0300

    Adicionar arquivos de projeto.

---
+++ Apostila/Controllers/LerApostilaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeituraApostila.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LerApostilaController : ControllerBase
+    {
+        public LerApostilaController()
+        {
+        }
+
+        private readonly ILogger<LerApostilaController> _logger;
+
+        public LerApostilaController(ILogger<LerApostilaController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet(Name = "GetLerArquivo")]
+        public IEnumerable<LerArquivoResponse> Get()
+        {
+            return null;
+        }
+    }
+}
